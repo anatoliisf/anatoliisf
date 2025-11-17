df7840a8be9fa9330b672d3e3d88080977e8fb80
Nate Kurochkin <nkurochkin@roblox.com>
2025-11-17 14:55:22 -0800

    Update links, attach updated resume

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -10,7 +10,7 @@ import { Resume } from './icons/Resume';
 
 const socialMediaList = [
     { id: 'resume', Component: Resume, url: '/docs/anatolii-kurochkin-resume.pdf' },
-    { id: 'github', Component: GitHub, url: 'https://github.com/anatoliik-lyft' },
+    { id: 'github', Component: GitHub, url: 'https://github.com/anatoliisf' },
     { id: 'linkedin', Component: Linkedin, url: 'https://www.linkedin.com/in/anatoliisf' },
     { id: 'instagram', Component: Instagram, url: 'https://www.instagram.com/anatoliisf' },
     { id: 'facebook', Component: Facebook, url: 'https://www.facebook.com/anatoliisf' },
@@ -22,7 +22,7 @@ const Main = () => {
             <S.Title>Anatolii Kurochkin</S.Title>
             <S.Subtitle>Software Engineer</S.Subtitle>
             <S.Face>
-                <S.FaceImg priority src={faceImage} alt="Avatar" />
+                <S.FaceImg src={faceImage} alt="Avatar" />
             </S.Face>
             <S.SocialMediaList>
                 {socialMediaList.map(({ id, Component, url }) => {
