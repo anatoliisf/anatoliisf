285a95b159c1fd3e1ed5a611f7965524dd97ba95
Nate Kurochkin <nkurochkin@roblox.com>
2025-11-17 23:08:26 -0800

    Fix build

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Dialog, DialogActions, DialogContent, DialogTitle, DialogWindow } from './Dialog';
-export { default as Dropdown } from './Dropdown';
-export { default as LangSelect } from './LangSelect';
